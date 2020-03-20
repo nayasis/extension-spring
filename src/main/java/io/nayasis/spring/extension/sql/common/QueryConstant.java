7461781031fa66ea1977cb21da6d83dcb2a9c45f
nayasis <nayasis@gmail.com>
2020-03-20 18:20:25 +0900

    change '.' character to '/'.

--- src/main/java/io/nayasis/spring/extension/sql/common/QueryConstant.java
+++ src/main/java/io/nayasis/spring/extension/sql/common/QueryConstant.java
@@ -2,7 +2,7 @@ package io.nayasis.spring.extension.sql.common;
 
 public class QueryConstant {
 
-    public static final String PARAMETER_SINGLE = "_nayasis.spring-extension.db.parameter.single";
-    public static final String FOR_EACH_INDEX   = "_nayasis.spring-extension.db.key.foreach";
+    public static final String PARAMETER_SINGLE = "_nayasis/spring-extension/db/parameter/single";
+    public static final String FOR_EACH_INDEX   = "_nayasis/spring-extension/db/key/foreach";
 
 }
