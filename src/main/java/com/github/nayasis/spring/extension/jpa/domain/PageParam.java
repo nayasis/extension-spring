45ca3042a39a52717829176241f8e119f334f91c
nayasis <nayasis@gmail.com>
2020-10-05 13:07:54 +0900

    add page parameter for API

---
+++ src/main/java/com/github/nayasis/spring/extension/jpa/domain/PageParam.java
@@ -0,0 +1,33 @@
+package com.github.nayasis.spring.extension.jpa.domain;
+
+import com.fasterxml.jackson.annotation.JsonSetter;
+import lombok.Data;
+import lombok.experimental.Accessors;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+@Data
+@Accessors(fluent=true)
+public class PageParam {
+
+    private int  page = 1;
+    private int  size = 100;
+    private Sort sort = Sort.unsorted();
+
+    public PageParam sort( Sort sort ) {
+        this.sort = sort;
+        return this;
+    }
+
+    @JsonSetter
+    public PageParam sort( String sort ) {
+        this.sort = SortBuilder.toSort(sort);
+        return this;
+    }
+
+    public Pageable toPageable() {
+        return PageRequest.of( page, size, sort );
+    }
+
+}
